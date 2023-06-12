d80be0b133c32d63cae3290cebc73be90b6d4c7c
Emmanuel <116069729+Rguzman77@users.noreply.github.com>
2023-06-12 11:48:39 +0200

    Added about text content and style

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,6 +1,19 @@
+import { AboutStyle } from "./AboutStyle"
+
 const About = () => {
   return (
-    <div>About</div>
+    <AboutStyle>
+      <p> 
+          Hello, I'm Emmanuel Guzman, a dedicated full stack developer specializing in Typescript and testing. With a comprehensive background in web development, I possess a wide range of skills and expertise to drive successful projects.
+          I find immense satisfaction in creating efficient and scalable applications. My proficiency in Typescript allows me to deliver clean and maintainable code, ensuring robustness and flexibility. Additionally, my expertise in testing ensures the reliability and stability of the applications I develop.
+          One notable achievement in my portfolio is the development of a Content Management System (CMS) for Enlace Urbano, an influential non-governmental organization. This CMS empowered the organization to efficiently manage and update their website content, enabling them to effectively communicate their mission and impact to a broader audience.
+          I have also been fortunate to contribute to a diverse range of projects. As part of a collaborative team, I helped develop a mobile application for a transportation company. Our innovative solution streamlined the booking process, enhancing user experience and overall customer satisfaction.
+          I am deeply driven by my desire to be part of a team that innovates and creates solutions that have a tangible impact. My ultimate goal is to utilize my skills and expertise to develop transformative solutions that address real-world challenges.
+          I eagerly look forward to connecting with fellow professionals who share my passion for technology and exploring opportunities to collaborate on exciting projects that push the boundaries of innovation.
+          <br/>
+          Let's join forces and make a difference together.
+      </p>
+    </AboutStyle>
   )
 }
 
---
+++ src/pages/about/AboutStyle.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const AboutStyle=styled.div`
+
+p{
+    margin: 15vh auto;
+    width: 80vw;
+    height: 50vh;
+    color: var(--color-text);
+    font-weight:400;
+    font-size: larger;
+    text-align: justify;
+    letter-spacing:0.04em;
+    line-height:4vh;
+}
+`
